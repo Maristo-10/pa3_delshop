59061905821147cb09a31a9bd2b82bf2757b7b02
Maristo-10 <maristositoruz@gmail.com>
2023-06-06 16:18:44 +0700

    dahboard

--- resources/views/layouts/frontend-pembeli.blade.php
+++ resources/views/layouts/frontend-pembeli.blade.php
@@ -44,7 +44,7 @@
     <link href="{{ asset('css/a.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/css/templatemo.css') }}" rel="stylesheet">
     {{-- <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet"> --}}
-    @stack('styles');
+    @stack('styles')
 </head>
 
 <body>
