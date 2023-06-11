2c36afca11a8fdc81b8ba203089e0c03508718f2
Maristo-10 <maristositoruz@gmail.com>
2023-06-11 20:30:37 +0700

    update dashboard

--- resources/views/component/checkout.blade.php
+++ resources/views/component/checkout.blade.php
@@ -99,7 +99,7 @@
                                 </form>
 
                                 <!--Modal-->
-                                <div class="modal fade" id="exampleModal{{ $item->id }}" tabindex="-1"
+                                <div class="modal fade" id="exampleModal{{ $ite m->id }}" tabindex="-1"
                                     aria-labelledby="exampleModalLabel" aria-hidden="true">
                                     <div class="modal-dialog">
                                         <div class="modal-content">
